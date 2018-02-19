2043911502e350d41f6bcfc8a746d3f720b323bd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-02-19 05:53:13 -0600

    pep8

--- src/nti/recipes/json/tests/test_recipe.py
+++ src/nti/recipes/json/tests/test_recipe.py
@@ -14,68 +14,68 @@ from unittest import TestCase
 
 from .. import Recipe
 
+
 class TestRecipe(TestCase):
 
-	def setUp(self):
-		self.name = 'test'
-		self.buildout = {}
-		self.buildout[self.name] = {}
-		self.buildout[self.name]['output-file'] = 'foo.json'
-		self.buildout[self.name]['contents-section'] = 'test-main'
-		self.buildout[self.name]['recipe'] = 'nti.recipe.json'
-		contents_section = self.buildout[self.name]['contents-section']
-		self.buildout[contents_section] = {}
-		self.buildout[contents_section]['foo'] = "foo"
-		self.buildout[contents_section]['bar'] = "bar"
-		self.buildout[contents_section]['baz-section'] = "test-baz"
-		self.buildout[contents_section]['bazbaz-section'] = "test-bazbaz"
-		baz_section = self.buildout[contents_section]['baz-section']
-		self.buildout[baz_section] = {}
-		self.buildout[baz_section]['foo'] = "foo"
-		self.buildout[baz_section]['bar'] = "bar"
-		self.buildout[baz_section]['baz'] = "baz"
-		self.buildout[baz_section]['foobar'] = """line 1
-		test-bazbaz-section
-		line 3
-		line 4"""
-		bazbaz_section = self.buildout[contents_section]['bazbaz-section']
-		self.buildout[bazbaz_section] = {}
-		self.buildout[bazbaz_section]['foo'] = "myfoo"
+    def setUp(self):
+        self.name = 'test'
+        self.buildout = {}
+        self.buildout[self.name] = {}
+        self.buildout[self.name]['output-file'] = 'foo.json'
+        self.buildout[self.name]['contents-section'] = 'test-main'
+        self.buildout[self.name]['recipe'] = 'nti.recipe.json'
+        contents_section = self.buildout[self.name]['contents-section']
+        self.buildout[contents_section] = {}
+        self.buildout[contents_section]['foo'] = "foo"
+        self.buildout[contents_section]['bar'] = "bar"
+        self.buildout[contents_section]['baz-section'] = "test-baz"
+        self.buildout[contents_section]['bazbaz-section'] = "test-bazbaz"
+        baz_section = self.buildout[contents_section]['baz-section']
+        self.buildout[baz_section] = {}
+        self.buildout[baz_section]['foo'] = "foo"
+        self.buildout[baz_section]['bar'] = "bar"
+        self.buildout[baz_section]['baz'] = "baz"
+        self.buildout[baz_section]['foobar'] = """line 1
+        test-bazbaz-section
+        line 3
+        line 4"""
+        bazbaz_section = self.buildout[contents_section]['bazbaz-section']
+        self.buildout[bazbaz_section] = {}
+        self.buildout[bazbaz_section]['foo'] = "myfoo"
 
-	def test_recipe( self ):
-		recipe = Recipe(self.buildout, self.name, self.buildout[self.name])
-		assert_that( recipe.contents, has_entry('foo', 'foo'))
-		assert_that( recipe.contents, has_entry('bar', 'bar'))
-		baz = recipe.contents['baz']
-		assert_that( baz, has_entry('foo', 'foo'))
-		assert_that( baz, has_entry('bar', 'bar'))
-		assert_that( baz, has_entry('baz', 'baz'))
-		bazbaz = recipe.contents['baz']['foobar'][1]
-		assert_that( bazbaz, has_entry('foo', 'myfoo'))
+    def test_recipe(self):
+        recipe = Recipe(self.buildout, self.name, self.buildout[self.name])
+        assert_that(recipe.contents, has_entry('foo', 'foo'))
+        assert_that(recipe.contents, has_entry('bar', 'bar'))
+        baz = recipe.contents['baz']
+        assert_that(baz, has_entry('foo', 'foo'))
+        assert_that(baz, has_entry('bar', 'bar'))
+        assert_that(baz, has_entry('baz', 'baz'))
+        bazbaz = recipe.contents['baz']['foobar'][1]
+        assert_that(bazbaz, has_entry('foo', 'myfoo'))
 
-	def test_install( self ):
-		recipe = Recipe(self.buildout, self.name, self.buildout[self.name])
-		filename = recipe.filename
+    def test_install(self):
+        recipe = Recipe(self.buildout, self.name, self.buildout[self.name])
+        filename = recipe.filename
 
-		if os.path.exists(filename):
-			os.unlink(filename)
+        if os.path.exists(filename):
+            os.unlink(filename)
 
-		recipe.install()
-		assert_that(os.path.exists(filename), is_(True))
-		
-		if os.path.exists(filename):
-			os.unlink(filename)
+        recipe.install()
+        assert_that(os.path.exists(filename), is_(True))
 
-	def test_update( self ):
-		recipe = Recipe(self.buildout, self.name, self.buildout[self.name])
-		filename = recipe.filename
+        if os.path.exists(filename):
+            os.unlink(filename)
 
-		if os.path.exists(filename):
-			os.unlink(filename)
+    def test_update(self):
+        recipe = Recipe(self.buildout, self.name, self.buildout[self.name])
+        filename = recipe.filename
 
-		recipe.update()
-		assert_that(os.path.exists(filename), is_(True))
-		
-		if os.path.exists(filename):
-			os.unlink(filename)
+        if os.path.exists(filename):
+            os.unlink(filename)
 
+        recipe.update()
+        assert_that(os.path.exists(filename), is_(True))
+
+        if os.path.exists(filename):
+            os.unlink(filename)
