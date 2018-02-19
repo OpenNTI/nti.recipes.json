abfeb249d46e46c8f7f89bb02d6cf11361a5cd34
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-02-19 05:52:21 -0600

    add from __future__ options

--- src/nti/recipes/json/tests/test_recipe.py
+++ src/nti/recipes/json/tests/test_recipe.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# pylint: disable=protected-access,too-many-public-methods,arguments-differ
 
 import os
 
